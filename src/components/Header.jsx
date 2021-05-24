fb9856b01248043105ac9f7a3b951971c370bbdb
Poonam-raj <p_rajput@hotmail.co.uk>
2021-05-24 17:37:22 +0100

    adding link to title of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <header className='Header'>
-      <h1>Northcoders News</h1>
+      <Link to='/' className='Header__title'>
+        <h1>Northcoders News</h1>
+      </Link>
       <p>Social news and CRUDdy content</p>
     </header>
   );

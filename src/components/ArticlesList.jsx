d47b74d69a6555990bd2c0afb4a00c14e503b3c5
Poonam-raj <p_rajput@hotmail.co.uk>
2021-05-24 16:14:36 +0100

    adding articles into List component, full list and filtering capability with useState and params

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { getArticles } from '../utils/api';
+import { ListOrder } from './ListOrder';
+
 const ArticlesList = () => {
+  const [articles, setArticles] = useState([]);
+
+  const { topic } = useParams();
+
+  useEffect(() => {
+    getArticles(topic).then((response) => {
+      setArticles(response);
+    });
+  }, [topic]);
+
   return (
-    <div>
-      <h2>All Articles</h2>
-      <ul>
-        <li>Article...</li>
-        <li>Article...</li>
-        <li>Article...</li>
-      </ul>
-    </div>
+    <main>
+      <h2 className='ArticlesList__header'>All Articles</h2>
+      <ListOrder />
+      <div>
+        {articles.map((article) => {
+          return (
+            <div key={article.article_id} className='ArticlesList__article'>
+              <div className='ArticlesList__article__details'>
+                <p>{article.author}</p>
+                <p>{article.created_at}</p>
+              </div>
+
+              <h3>{article.title}</h3>
+              <div className='ArticlesList__article__stats'>
+                <p>{article.votes} votes 💟 </p>
+                <p>{article.comment_count} comments 💬 </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </main>
   );
 };
 export default ArticlesList;

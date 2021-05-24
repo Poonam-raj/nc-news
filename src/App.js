f2a5c64c11f6765a2298cd70c09f702a067156e6
Poonam-raj <p_rajput@hotmail.co.uk>
2021-05-24 15:06:37 +0100

    app set up and basic css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Article from './components/Article';
+import ArticlesList from './components/ArticlesList';
+import Nav from './components/Nav';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Nav />
+      <Switch>
+        <Route exact path='/:author_id/:article_id'>
+          <Article />
+        </Route>
+        <Route exact path='/'>
+          <ArticlesList />
+        </Route>
+        <Route exact path='/topics/:topic'>
+          <ArticlesList />
+        </Route>
+      </Switch>
     </div>
   );
 }

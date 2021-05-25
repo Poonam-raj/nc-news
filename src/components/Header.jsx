7f38a985cb59d86525a70590fcfc8649865ba75d
Poonam-raj <p_rajput@hotmail.co.uk>
2021-05-25 14:22:07 +0100

    resetting querystring state on click of home link on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ setQueryString }) => {
   return (
     <header className='Header'>
-      <Link to='/' className='Header__title'>
+      <Link
+        to='/'
+        className='Header__title'
+        onClick={() => {
+          setQueryString({});
+        }}
+      >
         <h1>Northcoders News</h1>
       </Link>
       <p className='Header__subheader'>Social news and CRUD-dy content</p>

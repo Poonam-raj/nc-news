c1270469bebfcf52f920156fb4edb8457863b5e0
Poonam-raj <p_rajput@hotmail.co.uk>
2021-05-25 09:24:21 +0100

    adding simpler bootstrap dropdownButton to sortBy and Order By functions in List

--- src/components/ListOrder.jsx
+++ src/components/ListOrder.jsx
@@ -1,35 +1,26 @@
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 
 export const ListOrder = () => {
   return (
     <div className='ArticlesList__options'>
-      <Dropdown className='ArticlesList__sort-by'>
-        <Dropdown.Toggle
-          variant='success'
-          id='dropdown-basic'
-          className='ArticlesList__options__header'
-        >
-          Sort by
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          <Dropdown.Item href='#/action-1'>Date Created</Dropdown.Item>
-          <Dropdown.Item href='#/action-2'>Comment Count</Dropdown.Item>
-          <Dropdown.Item href='#/action-3'>Votes</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-      <Dropdown className='ArticlesList__sort-by'>
-        <Dropdown.Toggle
-          variant='success'
-          id='dropdown-basic'
-          className='ArticlesList__options__header'
-        >
-          Order by
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          <Dropdown.Item href='#/action-1'>Ascending</Dropdown.Item>
-          <Dropdown.Item href='#/action-2'>Descending</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+      <DropdownButton
+        id='dropdown-basic-button'
+        title='Sort by'
+        className='ArticlesList__sort-by'
+      >
+        <Dropdown.Item href='#/action-1'>Date Created</Dropdown.Item>
+        <Dropdown.Item href='#/action-2'>Comment Count</Dropdown.Item>
+        <Dropdown.Item href='#/action-3'>Votes</Dropdown.Item>
+      </DropdownButton>
+      <DropdownButton
+        id='dropdown-basic-button'
+        title='Order by'
+        className='ArticlesList__order-by'
+      >
+        <Dropdown.Item href='#/action-1'>Ascending</Dropdown.Item>
+        <Dropdown.Item href='#/action-2'>Descending</Dropdown.Item>
+      </DropdownButton>
     </div>
   );
 };

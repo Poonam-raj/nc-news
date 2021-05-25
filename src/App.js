5597f1dbb0e4ce6d19450f3163f7e062d59851d3
Poonam-raj <p_rajput@hotmail.co.uk>
2021-05-25 14:15:47 +0100

    adding props into components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './styles/App.css';
+import { useState } from 'react';
 import Header from './components/Header';
 import Article from './components/Article';
 import ArticlesList from './components/ArticlesList';
@@ -7,16 +8,23 @@ import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [queryString, setQueryString] = useState({});
   return (
     <div className='App'>
-      <Header />
-      <Nav />
+      <Header setQueryString={setQueryString} />
+      <Nav setQueryString={setQueryString} />
       <Switch>
         <Route exact path='/topics/:topic'>
-          <ArticlesList />
+          <ArticlesList
+            queryString={queryString}
+            setQueryString={setQueryString}
+          />
         </Route>
         <Route exact path='/'>
-          <ArticlesList />
+          <ArticlesList
+            queryString={queryString}
+            setQueryString={setQueryString}
+          />
         </Route>{' '}
         <Route exact path='/:author_id/:article_id'>
           <Article />
@@ -27,3 +35,7 @@ function App() {
 }
 
 export default App;
+
+//Articles need to remain filtered to the topic when the page is refreshed (path stays topic-specific)
+//dropdowns need to close when clicked
+//tidy components so states are shared more concisely

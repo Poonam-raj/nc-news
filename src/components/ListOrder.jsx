4343f74d1d65f5b461cebaf99b2e50d41b3b2d4c
Poonam-raj <p_rajput@hotmail.co.uk>
2021-05-27 17:10:00 +0100

    adding sortedBy messaging to indicate the current sort ordering

--- src/components/ListOrder.jsx
+++ src/components/ListOrder.jsx
@@ -1,9 +1,8 @@
+import { useState } from 'react';
 import { setQuery } from '../utils/util';
 
 /*
   TODO
-
-  - a way of knowing if a order has been selected - visual indication of active sorting order
     - dropdowns need to close when clicked
 */
 
@@ -13,45 +12,64 @@ export const ListOrder = ({ setQueryString }) => {
 
   const orderBySlug = ['asc', 'desc'];
   const orderByName = ['Ascending', 'Descending'];
+  const [sortedFilterMsg, setSortedFilterMsg] = useState({
+    sort: 'Date Created',
+    order: 'Descending'
+  });
 
   return (
-    <div className='ArticlesList__options'>
-      <button className='dropdown'>
-        Sort By
-        <ul className='dropdown-content'>
-          {sortBySlug.map((item, i) => {
-            return (
-              <li
-                className='dropdown__item'
-                key={item}
-                onClick={() => {
-                  setQuery(setQueryString, 'sort_by', item);
-                }}
-              >
-                {sortByName[i]}
-              </li>
-            );
-          })}
-        </ul>
-      </button>
-      <button className='dropdown'>
-        Order by
-        <ul className='dropdown-content'>
-          {orderBySlug.map((item, i) => {
-            return (
-              <li
-                className='dropdown__item'
-                key={item}
-                onClick={() => {
-                  setQuery(setQueryString, 'order', item);
-                }}
-              >
-                {orderByName[i]}
-              </li>
-            );
-          })}
-        </ul>
-      </button>
-    </div>
+    <>
+      <div className='ListOrder__options'>
+        <button className='dropdown'>
+          Sort By
+          <ul className='dropdown-content'>
+            {sortBySlug.map((item, i) => {
+              return (
+                <li
+                  className='dropdown__item'
+                  key={item}
+                  onClick={() => {
+                    setQuery(setQueryString, 'sort_by', item);
+                    setSortedFilterMsg((currMsg) => {
+                      const newMsg = { ...currMsg, sort: sortByName[i] };
+                      return newMsg;
+                    });
+                  }}
+                >
+                  {sortByName[i]}
+                </li>
+              );
+            })}
+          </ul>
+        </button>
+        <button className='dropdown'>
+          Order by
+          <ul className='dropdown-content'>
+            {orderBySlug.map((item, i) => {
+              return (
+                <li
+                  className='dropdown__item'
+                  key={item}
+                  onClick={() => {
+                    setQuery(setQueryString, 'order', item);
+                    setSortedFilterMsg((currMsg) => {
+                      const newMsg = { ...currMsg, order: orderByName[i] };
+                      return newMsg;
+                    });
+                  }}
+                >
+                  {orderByName[i]}
+                </li>
+              );
+            })}
+          </ul>
+        </button>
+      </div>{' '}
+      <div className='ListOrder__sort-statement-container'>
+        <p className='ListOrder__sort-statement'>
+          Sorted by {sortedFilterMsg.sort}, in {sortedFilterMsg.order} order.
+        </p>
+      </div>
+    </>
   );
 };
